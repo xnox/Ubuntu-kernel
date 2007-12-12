5a63a5534582ecb2ea5ff0da68c955774618ad4e
Tim Gardner <tim.gardner@canonical.com>
2007-12-12 11:52:00 -0700

    UBUNTU: Disable -rt, -ume, -rt custom binary builds. Ignore: yes
    Disable the custom binaries until the kernel catches up.
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- debian/rules.d/amd64.mk
+++ debian/rules.d/amd64.mk
@@ -1,3 +1,4 @@
 build_arch	= x86_64
 
-flavours	= generic server rt xen
+#flavours	= generic server rt xen
+flavours	= generic server
--- debian/rules.d/i386.mk
+++ debian/rules.d/i386.mk
@@ -1,2 +1,3 @@
 build_arch	= i386
-flavours	= 386 generic server xen ume rt virtual
+#flavours	= 386 generic server xen ume rt virtual
+flavours	= 386 generic server virtual
