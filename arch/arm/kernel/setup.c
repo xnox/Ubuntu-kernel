1f2f114f4d0f28aa2c842d0feb2b2d4416744009
Jason Wessel <jason.wessel@windriver.com>
2008-02-20 13:33:40 -0600

    kgdb: support for ARCH=arm
    This patch adds the ARCH=arm specific a kgdb backend, originally
    written by Deepak Saxena <dsaxena@plexity.net> and George Davis
    <gdavis@mvista.com>.  Geoff Levand <geoffrey.levand@am.sony.com>,
    Nicolas Pitre, and Manish Lachwani have contributed various fixups
    here as well.
    
    The changes to setup the traps earlier allow for early debugging with
    a uart based KGDB I/O driver.  The do_undefinstr() routine also needed
    to allow the lookup of kernel address space in order for the debugger
    to plant undefined instructions in kernel memory space and receive the
    correct notification.
    
    [ mingo@elte.hu: small cleanups. ]
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -37,6 +37,7 @@
 #include <asm/mach/arch.h>
 #include <asm/mach/irq.h>
 #include <asm/mach/time.h>
+#include <asm/traps.h>
 
 #include "compat.h"
 
@@ -879,6 +880,10 @@ void __init setup_arch(char **cmdline_p)
 #elif defined(CONFIG_DUMMY_CONSOLE)
 	conswitchp = &dummy_con;
 #endif
+#endif
+
+#if defined(CONFIG_KGDB)
+	early_trap_init();
 #endif
 }
 
