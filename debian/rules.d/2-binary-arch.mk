ca7c70fe85c7995279b3d333a3e44b44e9cbc645
Tim Gardner <tim.gardner@canonical.com>
2008-03-03 12:35:11 -0700

    UBUNTU: Create arch headers packages. Ignore: yes
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -43,11 +43,15 @@ $(stampdir)/stamp-build-%: $(stampdir)/stamp-prepare-%
 install-%: pkgdir = $(CURDIR)/debian/linux-ubuntu-modules-$(release)-$(abinum)-$*
 install-%: moddir = $(pkgdir)/lib/modules/$(release)-$(abinum)-$*
 install-%: firmdir = $(pkgdir)/lib/firmware/$(release)-$(abinum)-$*
+install-%: basehdrpkg = linux-headers-lum-$(release)$(debnum)
+install-%: hdrpkg = $(basehdrpkg)-$*
+install-%: hdrdir = $(CURDIR)/debian/$(hdrpkg)/usr/src/$(hdrpkg)
 install-%: target_flavour = $*
 install-%: $(stampdir)/stamp-build-%
 	dh_testdir
 	dh_testroot
 	dh_clean -k -plinux-ubuntu-modules-$(release)-$(abinum)-$*
+	dh_clean -k -p$(hdrpkg)
 
 	install -d $(firmdir)
 	cp -a ubuntu-firmware/zd1211 $(firmdir)/
@@ -72,6 +76,20 @@ endif
 	       debian/control-scripts/$$script > $(pkgdir)/DEBIAN/$$script;	\
 	  chmod 755 $(pkgdir)/DEBIAN/$$script;					\
 	done
+
+	# The flavour specific headers package
+	install -d $(hdrdir)/sound
+	cp `find $(builddir)/build-$*/sound/alsa-kernel/include -type f` $(hdrdir)/sound
+	dh_testdir
+	dh_testroot
+	dh_installchangelogs -p$(hdrpkg)
+	dh_installdocs -p$(hdrpkg)
+	dh_compress -p$(hdrpkg)
+	dh_fixperms -p$(hdrpkg)
+	dh_installdeb -p$(hdrpkg)
+	dh_gencontrol -p$(hdrpkg)
+	dh_md5sums -p$(hdrpkg)
+	dh_builddeb -p$(hdrpkg)
 
 binary-modules-%: pkgimg = linux-ubuntu-modules-$(release)-$(abinum)-$*
 binary-modules-%: install-%
