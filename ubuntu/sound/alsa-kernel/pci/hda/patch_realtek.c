5c3fad830d68d68bb9550019ac5f8269064c6915
Michael Frey (Senior Manager, MID) <michael.frey@canonical.com>
2008-07-16 16:07:45 +0100

    Added a PCI Quirk for CW020 codec Signed-off-by: Michael Frey (Senior Manager, MID) <michael.frey@canonical.com>

--- ubuntu/sound/alsa-kernel/pci/hda/patch_realtek.c
+++ ubuntu/sound/alsa-kernel/pci/hda/patch_realtek.c
@@ -10597,7 +10597,8 @@ static const char *alc269_models[ALC269_MODEL_LAST] = {
 };
 
 static struct snd_pci_quirk alc269_cfg_tbl[] = {
-	{}
+        SND_PCI_QUIRK(0x8086,0x811b, "CW020", ALC269_CW020),
+	{ }
 };
 
 static struct alc_config_preset alc269_presets[] = {
