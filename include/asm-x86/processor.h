89fa64f9e66b659be434e0861dafa125ecd05f5e
Loïc Minier <lool@dooz.org>
2008-11-17 22:52:45 +0100

    Misc fixes for kgdb build
    - add kgdb config
    - include asm-x86/processor.h
    - fix asm-x86/processor.h to always export early_init
    - fix 0001-dabney_thermal patch to apply against the config with KGDB
      config
    - fix reg names

--- include/asm-x86/processor.h
+++ include/asm-x86/processor.h
@@ -1,7 +1,7 @@
-#ifdef CONFIG_X86_32
+extern void early_trap_init(void);
+
+#if defined(CONFIG_X86_32)
 # include "processor_32.h"
 #else
 # include "processor_64.h"
-extern void early_trap_init(void);
-
 #endif
