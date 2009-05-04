4e8a7f1e463731786f1c94807c976e79c114bd61
Stefan Bader <stefan.bader@canonical.com>
2009-05-04 23:31:37 +0200

    UBUNTU: buildenv: Adapt the variables to LRM versioning scheme
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- debian/rules.d/0-common-vars.mk
+++ debian/rules.d/0-common-vars.mk
@@ -1,8 +1,13 @@
 # Rip some version information from our changelog
-release	:= $(shell sed -n '1s/^.*(\(.*\)\..*-.*).*$$/\1/p' debian/changelog)
-revisions := $(shell sed -n 's/^linux-restricted-modules-2.6.24\ .*($(release)\.\(.*-.*\)).*$$/\1/p' debian/changelog | tac)
-revision ?= $(word $(words $(revisions)),$(revisions))
-prev_revisions := $(filter-out $(revision),0.0 $(revisions))
-prev_revision := $(word $(words $(prev_revisions)),$(prev_revisions))
 #  
 gitver=$(shell if test -f .git/HEAD; then cat .git/HEAD; else uuidgen; fi)
+
+pkgname           := $(shell dpkg-parsechangelog | awk '/^Source/{print $$2}')
+
+abi_version        = $(shell dpkg-parsechangelog | grep ^Version | sed 's/.*-//;s/\..*//')
+kernel_version     = $(shell dpkg-parsechangelog | grep ^Source | sed 's/.*-//')
+kernel_abi_version = $(kernel_version)-$(abi_version)
+lrm_version        = $(shell dpkg-parsechangelog | awk '/Version:/{print $$2}')
+lrm_versions      := $(shell sed -n 's/^$(pkgname)\ .*(\(.*\)).*$$/\1/p' debian/changelog | tac)
+prev_lrm_versions := $(filter-out $(lrm_version),0.0 $(lrm_versions))
+prev_lrm_version  := $(word $(words $(prev_lrm_versions)),$(prev_lrm_versions))
