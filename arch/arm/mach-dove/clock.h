6a3a81d78c505035a0bb75f25463b48240c7aa30
Saeed Bishara <saeed@marvell.com>
2009-05-21 20:00:16 +0300

    dove: impelement the clkdev framework for the GPU and AXI clocks
    move the gpu and axi clock's stuff from common.c to the clock.c file

--- arch/arm/mach-dove/clock.h
+++ arch/arm/mach-dove/clock.h
@@ -2,7 +2,7 @@ struct clkops {
 	void			(*enable)(struct clk *);
 	void			(*disable)(struct clk *);
 	unsigned long		(*getrate)(struct clk *);
-	unsigned long		(*setrate)(struct clk *);
+	int			(*setrate)(struct clk *, unsigned long rate);
 };
 
 struct clk {
@@ -18,4 +18,5 @@ struct clk {
 
 int dove_devclks_init(void);
 unsigned int  dove_tclk_get(void);
+int dove_clk_config(struct device *dev, const char *id, unsigned long rate);
 void ds_clks_disable_all(int include_pci0, int include_pci1);
