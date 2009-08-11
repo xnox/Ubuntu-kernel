44dbf4ef187a7e0823f5ae60bddaa791ebff2d3e
Shadi Ammouri <shadi@marvell.com>
2009-08-11 09:20:23 +0300

    Add support for gagned mode and multi chip-select
    - Bug fixes.
    - Code cleanup.
    - BCH support.
    
    Signed-off-by: Shadi Ammouri <shadi@marvell.com>
    Signed-off-by: Saeed Bishara <saeed@marvell.com>

--- arch/arm/mach-dove/include/mach/dove_nand.h
+++ arch/arm/mach-dove/include/mach/dove_nand.h
@@ -7,6 +7,10 @@
 struct dove_nand_platform_data {
 	unsigned int		tclk;		/* Clock supplied to NFC */
 	unsigned int		nfc_width;	/* Width of NFC 16/8 bits */
+	unsigned int		num_devs;	/* Number of NAND devices 
+						   (2 for ganged mode).   */
+	unsigned int		num_cs;		/* Number of NAND devices 
+						   chip-selects.	  */
 	unsigned int		use_dma;	/* Enable/Disable DMA 1/0 */
 	unsigned int 		use_ecc;	/* Enable/Disable ECC 1/0 */
 	unsigned int		use_bch;	/* Enable/Disable BCH 1/0 (if ECC enabled) */
