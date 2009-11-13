929a79ad0942bc36c1288eb526ffdc76987327e3
Tim Gardner <tim.gardner@canonical.com>
2009-11-13 09:06:30 -0700

    UBUNTU: Update to compat-wireless-2.6.32-rc7
    OriginalLocation: http://www.orbit-lab.org/kernel/compat-wireless-2.6-stable/v2.6.32/compat-wireless-2.6.32-rc7.tar.bz2
    BugLink: http://bugs.launchpad.net/bugs/481525
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- updates/compat-wireless-2.6/drivers/net/wireless/ipw2x00/ipw2100.c
+++ updates/compat-wireless-2.6/drivers/net/wireless/ipw2x00/ipw2100.c
@@ -6336,10 +6336,8 @@ static int ipw2100_pci_init_one(struct pci_dev *pci_dev,
 
       fail:
 	if (dev) {
-		if (registered) {
-			unregister_ieee80211(priv->ieee);
+		if (registered)
 			unregister_netdev(dev);
-		}
 
 		ipw2100_hw_stop_adapter(priv);
 
@@ -6396,7 +6394,6 @@ static void __devexit ipw2100_pci_remove_one(struct pci_dev *pci_dev)
 		/* Unregister the device first - this results in close()
 		 * being called if the device is open.  If we free storage
 		 * first, then close() will crash. */
-		unregister_ieee80211(priv->ieee);
 		unregister_netdev(dev);
 
 		/* ipw2100_down will ensure that there is no more pending work
