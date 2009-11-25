44b1ab882e966e57f6231adf7777050a7cc9a943
John Johansen <john.johansen@canonical.com>
2009-11-25 10:01:34 -0800

    UBUNTU: [Config] Setup EC2 commit
    Setup of debian.ec2 pulled forward from karmic
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: John Johansen <john.johansen@canonical.com>

---
+++ debian.ec2/rules.d/i386.mk
@@ -0,0 +1,9 @@
+build_arch	= x86
+header_arch	= x86
+asm_link	= x86
+defconfig	= defconfig
+flavours        = ec2
+build_image	= vmlinuz
+kernel_file	= arch/$(build_arch)/boot/vmlinuz
+install_file	= vmlinuz
+loader		= grub
