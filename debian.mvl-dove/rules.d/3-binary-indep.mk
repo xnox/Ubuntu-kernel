258c246173a4598d64b27af8c0c653230a9c55ef
Andy Whitcroft <apw@canonical.com>
2010-01-28 12:50:10 +0000

    UBUNTU: convert package disable to a deps list -- mvl-dove
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- debian.mvl-dove/rules.d/3-binary-indep.mk
+++ debian.mvl-dove/rules.d/3-binary-indep.mk
@@ -3,7 +3,6 @@ build-indep:
 docpkg = $(doc_pkg_name)
 docdir = $(CURDIR)/debian/$(docpkg)/usr/share/doc/$(docpkg)
 install-doc:
-ifeq ($(do_doc_package),true)
 	dh_testdir
 	dh_testroot
 	dh_clean -k -p$(docpkg)
@@ -23,12 +22,10 @@ endif
 	cp -a Documentation/* $(docdir)
 	rm -rf $(docdir)/DocBook
 	find $(docdir) -name .gitignore | xargs rm -f
-endif
 
 srcpkg = $(src_pkg_name)-source-$(release)
 srcdir = $(CURDIR)/debian/$(srcpkg)/usr/src/$(srcpkg)
 install-source:
-ifeq ($(do_source_package),true)
 	dh_testdir
 	dh_testroot
 	dh_clean -k -p$(srcpkg)
@@ -42,8 +39,14 @@ ifeq ($(do_linux_source_content),true)
 		$(srcdir).tar.bz2
 	rm -rf $(srcdir)
 endif
-endif
 
-install-indep: install-doc install-source
+install-indep-deps = 
+ifeq ($(do_doc_package),true)
+install-indep-deps += install-doc
+endif
+ifeq ($(do_source_package),true)
+install-indep-deps += install-doc
+endif
+install-indep: $(install-indep-deps)
 
 binary-indep: install-indep
