4c409f0393f955650f75a8c18025b354eec32028
Andy Whitcroft <apw@canonical.com>
2010-06-09 21:40:57 +0100

    UBUNTU: update to ubuntu-debian:7e708d33054c373faf41da23b73e8b48c342d958
      [ Andy Whitcroft ]
    
      * initial import of the debian machinery from Maverick
      * enforcer -- move configuration series specific
      * abi-check -- be more forgiving of the ABI element type
        - LP: #576274
      * debian -- fix modules.builtin naming after cleanup
      * debian -- fix distclean to be branch specific
      * pass LOCALVERSION= to kernel builds to suppress version suffix
      * debian -- add a full_build override
    
      [ Lee Jones ]
    
      * Enable perf to be more helpful when perf_<version> does not exist.
        - LP: #570500
      * 'fdr editconfig' modification. Easily skip over unwanted menuconfigs.
    
      [ Tim Gardner ]
    
      * [Config] Added module inclusion support
    
    BugLink: http://bugs.launchpad.net/bugs/576274
    BugLink: http://bugs.launchpad.net/bugs/570500
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -78,6 +78,17 @@ endif
 		INSTALL_MOD_STRIP=1 INSTALL_MOD_PATH=$(pkgdir)/ \
 		INSTALL_FW_PATH=$(pkgdir)/lib/firmware/$(abi_release)-$*
 
+	#
+	# Remove all modules not in the inclusion list.
+	#
+	if [ -f $(DEBIAN)/control.d/$(target_flavour).inclusion-list ] ; then \
+		$(DROOT)/scripts/module-inclusion $(pkgdir)/lib/modules/$(abi_release)-$*/kernel \
+			$(DEBIAN)/control.d/$(target_flavour).inclusion-list 2>&1 | \
+				tee $(target_flavour).inclusion-list.log; \
+		/sbin/depmod -b $(pkgdir) -ea -F $(pkgdir)/boot/System.map-$(abi_release)-$* \
+			$(abi_release)-$* 2>&1 |tee $(target_flavour).depmod.log; \
+	fi
+
 ifeq ($(no_dumpfile),)
 	makedumpfile -g $(pkgdir)/boot/vmcoreinfo-$(abi_release)-$* \
 		-x $(builddir)/build-$*/vmlinux
@@ -191,15 +202,17 @@ endif
 	# Remove files which are generated at installation by postinst,
 	# except for modules.order and modules.builtin
 	#
+	mkdir $(pkgdir)/lib/modules/$(abi_release)-$*/_
 	mv $(pkgdir)/lib/modules/$(abi_release)-$*/modules.order \
-		$(pkgdir)/lib/modules/$(abi_release)-$*/_modules.order
-	mv $(pkgdir)/lib/modules/$(abi_release)-$*/modules.builtin \
-		$(pkgdir)/lib/modules/$(abi_release)-$*/_modules.builtin
+		$(pkgdir)/lib/modules/$(abi_release)-$*/_
+	if [ -f $(pkgdir)/lib/modules/$(abi_release)-$*/modules.builtin ] ; then \
+	    mv $(pkgdir)/lib/modules/$(abi_release)-$*/modules.builtin \
+		$(pkgdir)/lib/modules/$(abi_release)-$*/_; \
+	fi
 	rm -f $(pkgdir)/lib/modules/$(abi_release)-$*/modules.*
-	mv $(pkgdir)/lib/modules/$(abi_release)-$*/_modules.builtin \
-		$(pkgdir)/lib/modules/$(abi_release)-$*/modules.builtin
-	mv $(pkgdir)/lib/modules/$(abi_release)-$*/_modules.order \
-		$(pkgdir)/lib/modules/$(abi_release)-$*/modules.order
+	mv $(pkgdir)/lib/modules/$(abi_release)-$*/_/* \
+		$(pkgdir)/lib/modules/$(abi_release)-$*
+	rmdir $(pkgdir)/lib/modules/$(abi_release)-$*/_
 
 headers_tmp := $(CURDIR)/debian/tmp-headers
 headers_dir := $(CURDIR)/debian/linux-libc-dev
