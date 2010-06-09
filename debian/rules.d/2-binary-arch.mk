5348684750a5f0b159604477dbdd862216478163
Chase Douglas <chase.douglas@canonical.com>
2010-06-09 12:09:49 -0400

    UBUNTU: Added iwlwifi 6050 series firmware
    http://intellinuxwireless.org/iwlwifi/downloads/iwlwifi-6050-ucode-9.201.4.1.tgz
    
    BugLink: http://bugs.launchpad.net/bugs/532451
    
    Signed-off-by: Chase Douglas <chase.douglas@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -94,6 +94,7 @@ install-%: $(stampdir)/stamp-build-%
 	cp firmware/iwlwifi/*5000*/*.ucode $(firmdir)/lbm-iwlwifi-5000-1.ucode
 	cp firmware/iwlwifi/*5150*/*.ucode $(firmdir)/lbm-iwlwifi-5150-1.ucode
 	cp firmware/iwlwifi/*6000*/*.ucode $(firmdir)/lbm-iwlwifi-6000-4.ucode
+	cp firmware/iwlwifi/*6050*/*.ucode $(firmdir)/lbm-iwlwifi-6050-4.ucode
 	cp firmware/rt28x0/rt2870.bin $(firmdir)/
 
 	#
