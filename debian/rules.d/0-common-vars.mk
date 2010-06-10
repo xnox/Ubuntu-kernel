508b7aa34b578c0d1e51bfb571f2bfb824dc65ac
Andy Whitcroft <apw@canonical.com>
2010-06-10 09:48:54 +0100

    UBUNTU: debian -- ensure the version number is clean
    As of v2.6.35-rc2 the kernel version generator sometimes appends a '+'
    to the overall version number based on the current state of the SCM.
    Forcibly override the version handling for this version.
    
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- debian/rules.d/0-common-vars.mk
+++ debian/rules.d/0-common-vars.mk
@@ -176,7 +176,7 @@ kmake = make ARCH=$(build_arch) \
 	EXTRAVERSION=-$(abinum)-$(target_flavour) \
 	CONFIG_DEBUG_SECTION_MISMATCH=y SUBLEVEL=$(SUBLEVEL) \
 	KBUILD_BUILD_VERSION="$(uploadnum)" \
-	LOCALVERSION=
+	LOCALVERSION= localver-extra=
 ifneq ($(LOCAL_ENV_CC),)
 kmake += CC=$(LOCAL_ENV_CC) DISTCC_HOSTS=$(LOCAL_ENV_DISTCC_HOSTS)
 endif
