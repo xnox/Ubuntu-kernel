8ca5dc75b68cffe72a1a691cc111dc1e0d3d0bbf
Tim Gardner <tim.gardner@canonical.com>
2010-07-28 08:32:43 -0600

    kernel-team-accounts: Create/update kteam-tools repo
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- kernel-team-accounts/update-repos.sh
+++ kernel-team-accounts/update-repos.sh
@@ -25,3 +25,12 @@ do
 	fi
 done
 
+#
+# Create and update a copy of the kteam-tools repo
+#
+if [ ! -d kteam-tools ]
+then
+	git clone ${UBUNTU}/kteam-tools.git kteam-tools
+else
+	(cd kteam-tools; git fetch origin;git fetch origin master;git reset --hard FETCH_HEAD)
+fi
