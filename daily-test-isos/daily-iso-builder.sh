5e9eefed844138c56c812cc5117a37435ec48c0e
Brad Figg <brad.figg@canonical.com>
2010-07-28 16:52:29 +0100

    1. Added a bunch of comments to the daily-iso-builder script and    pulled many of the constants into variables to make it easier    to customize.
    2. Additional packages are removed to help reduce the size of the
       isos.
    
    3. Pulled in Colin King's changes which add creating custom isos
       with his firmware test suite enabled.
    
       a. A command line switch is added to indicate the fwts tests
          are to be used.
    
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- daily-test-isos/daily-iso-builder.sh
+++ daily-test-isos/daily-iso-builder.sh
@@ -1,22 +1,67 @@
 #!/bin/bash
+#
+# This is the script that builds the isos every day.
+#
 set -e
-if [ -e kteam-tools ];then
-    rm -rf kteam-tools
-fi
 
-git clone git://kernel.ubuntu.com/ubuntu/kteam-tools.git
+BASE=/home/bradf               # Home directory of the user that is building the
+                               # isos.
 
-wget http://cdimage.ubuntu.com/daily-live/current/maverick-desktop-amd64.iso
-wget http://cdimage.ubuntu.com/daily-live/current/maverick-desktop-i386.iso
+REPOS=$BASE/repos              # Location of any git trees that are needed to build
+                               # the isos.
+
+KTEAM_REPO=$REPOS/kteam-tools  # This is the 'authoritative' copy of the kteam-tools.
+                               # It is only updated after careful review of the latest
+                               # changes.
+
+WORKING_DIR=$BASE/work         # The directory where the 'work' of building the isos
+                               # is done. This directory also contains the directory
+                               # that is mirrored onto zinc.
+
+git clone $KTEAM_REPO
+
+RELEASE=maverick
+DESKTOP_AMD64=$RELEASE-desktop-amd64
+DESKTOP_I386=$RELEASE-desktop-i386
+
+# Fetch the latest daily isos we are going to customize. Note, wget is being
+# used because this build is being done on a system in the DC and the network
+# hose to where the daily-isos are is HUGE.
+#
+wget http://cdimage.ubuntu.com/daily-live/current/$DESKTOP_AMD64.iso
+wget http://cdimage.ubuntu.com/daily-live/current/$DESKTOP_I386.iso
 
 (
     cd kteam-tools/daily-test-isos
+
+    # Fetch a copy of the kernel teams tests.
+    #
     git clone git://kernel.ubuntu.com/manjo/kernel-qa.git
-    rm -rf kernel-qa/.git
-    rm kernel-qa/tests/video
-    ./mk-custom-iso -i /home/bradf/work/maverick-desktop-i386.iso
-    ./mk-custom-iso -i /home/bradf/work/maverick-desktop-amd64.iso
+    rm -rf kernel-qa/.git    # This is removed to save space.
+    rm kernel-qa/tests/video # FIXME (bjf): removed because of changes to maverick's 
+                             #              userspace apps which prevented the test
+                             #              from working. Should be re-evaluated.
+
+    # Build the kernel team's standard, test isos
+    #
+    ./mk-custom-iso -i $WORKING_DIR/$DESKTOP_I386.iso
+    ./mk-custom-iso -i $WORKING_DIR/$DESKTOP_AMD64.iso
+
+    cp /tmp/$DESKTOP_I386-custom.iso  $WORKING_DIR/zinc-mirror/$DESKTOP_I386-ktts.iso
+    cp /tmp/$DESKTOP_AMD64-custom.iso $WORKING_DIR/zinc-mirror/$DESKTOP_AMD64-ktts.iso
+
+    # Build the krenel team's "firmware test suite" enabled test isos
+    #
+    ./mk-custom-iso -f -i $WORKING_DIR/$DESKTOP_I386.iso
+    ./mk-custom-iso -f -i $WORKING_DIR/$DESKTOP_AMD64.iso
+
+    cp /tmp/$DESKTOP_I386-custom.iso  $WORKING_DIR/zinc-mirror/$DESKTOP_I386-fwts.iso
+    cp /tmp/$DESKTOP_AMD64-custom.iso $WORKING_DIR/zinc-mirror/$DESKTOP_AMD64-fwts.iso
 )
-cp /tmp/maverick-desktop-i386-custom.iso  /home/bradf/work/zinc-mirror/
-cp /tmp/maverick-desktop-amd64-custom.iso /home/bradf/work/zinc-mirror/
+
+
+# Cleanup
+#
 rm -rf kteam-tools
+
+# vi:set ts=4 sw=4 expandtab:
