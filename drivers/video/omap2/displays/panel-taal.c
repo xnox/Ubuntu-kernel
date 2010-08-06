dc93aebc731a14e66d0b468dbe9cd3c9e6c3b135
archit taneja <archit@ti.com>
2010-08-06 18:46:33 +0530

    DSS2: DSI error recovery scheme
    DSI error recovery scheme introduced by scheduling a work
    thread once a error is detetced by the interrupt handler.

--- drivers/video/omap2/displays/panel-taal.c
+++ drivers/video/omap2/displays/panel-taal.c
@@ -1209,7 +1209,8 @@ static irqreturn_t taal_te_isr(int irq, void *data)
 	int r;
 
 	if (td->force_update) {
-		queue_work(td->te_wq, &td->te_framedone_work);
+		if (!omap_dsi_recovery_state(DSI1))
+			queue_work(td->te_wq, &td->te_framedone_work);
 		return IRQ_HANDLED;
 	}
 
@@ -1243,9 +1244,10 @@ static irqreturn_t taal_te_isr2(int irq, void *data)
 	int r;
 
 	if (td->force_update) {
-		queue_work(td->te_wq, &td->te_framedone_work);
-		return IRQ_HANDLED;
-	}
+		if (!omap_dsi_recovery_state(DSI2))
+			queue_work(td->te_wq, &td->te_framedone_work);
+                return IRQ_HANDLED;
+        }
 
 	old = atomic_cmpxchg(&td->do_update, 1, 0);
 
