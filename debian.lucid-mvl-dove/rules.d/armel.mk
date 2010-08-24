d0a727b4fe8668bbcec5d53e211966f451c074a4
Tim Gardner <tim.gardner@canonical.com>
2010-08-24 17:21:28 -0600

    UBUNTU: Created lucid-mvl-dove
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

---
+++ debian.lucid-mvl-dove/rules.d/armel.mk
@@ -0,0 +1,19 @@
+build_arch	= arm
+header_arch	= arm
+asm_link	= arm
+defconfig	= defconfig
+flavours	= dove
+build_image	= zImage
+kernel_file	= arch/$(build_arch)/boot/zImage
+install_file	= vmlinuz
+no_dumpfile     = true
+loader		= grub
+
+do_doc_package		= false
+do_source_package	= false
+do_common_headers_indep = false
+do_libc_dev_package	= false
+do_full_source		= true
+do_tools		= false
+skipabi			= true
+skipmodule		= true
