588a4f6462cca6951e538f48ab78620a5163e4f0
Bryan Wu <bryan.wu@canonical.com>
2010-08-26 13:52:59 +0800

    Ubuntu: SAUCE: sync debian packaging stuff from Ubuntu-2.6.34-903.7 release
    Signed-off-by: Bryan Wu <bryan.wu@canonical.com>

--- debian.master/rules.d/i386.mk
+++
@@ -1,9 +0,0 @@
-build_arch	= i386
-header_arch	= x86_64
-asm_link	= x86
-defconfig	= defconfig
-flavours        = generic generic-pae virtual
-build_image	= bzImage
-kernel_file	= arch/$(build_arch)/boot/bzImage
-install_file	= vmlinuz
-loader		= grub
