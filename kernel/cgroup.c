74eb20fe3158cec49770f93cb02cac9f7fa32e58
Colin Cross <ccross@android.com>
2010-11-23 21:37:03 -0800

    cgroup: Set CGRP_RELEASABLE when adding to a cgroup
    Changes the meaning of CGRP_RELEASABLE to be set on any cgroup
    that has ever had a task or cgroup in it, or had css_get called
    on it.  The bit is set in cgroup_attach_task, cgroup_create,
    and __css_get.  It is not necessary to set the bit in
    cgroup_fork, as the task is either in the root cgroup, in
    which can never be released, or the task it was forked from
    already set the bit in croup_attach_task.
    
    Signed-off-by: Colin Cross <ccross@android.com>
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -396,7 +396,15 @@ static struct hlist_head *css_set_hash(struct cgroup_subsys_state *css[])
  * compiled into their kernel but not actually in use */
 static int use_task_css_set_links __read_mostly;
 
-static void __put_css_set(struct css_set *cg, int taskexit)
+/*
+ * refcounted get/put for css_set objects
+ */
+static inline void get_css_set(struct css_set *cg)
+{
+	atomic_inc(&cg->refcount);
+}
+
+static void put_css_set(struct css_set *cg)
 {
 	struct cg_cgroup_link *link;
 	struct cg_cgroup_link *saved_link;
@@ -422,36 +430,14 @@ static void __put_css_set(struct css_set *cg, int taskexit)
 		struct cgroup *cgrp = link->cgrp;
 		list_del(&link->cg_link_list);
 		list_del(&link->cgrp_link_list);
-		if (atomic_dec_and_test(&cgrp->count) &&
-		    notify_on_release(cgrp)) {
-			if (taskexit)
-				set_bit(CGRP_RELEASABLE, &cgrp->flags);
+		if (atomic_dec_and_test(&cgrp->count))
 			check_for_release(cgrp);
-		}
 
 		kfree(link);
 	}
 
 	write_unlock(&css_set_lock);
 	kfree_rcu(cg, rcu_head);
-}
-
-/*
- * refcounted get/put for css_set objects
- */
-static inline void get_css_set(struct css_set *cg)
-{
-	atomic_inc(&cg->refcount);
-}
-
-static inline void put_css_set(struct css_set *cg)
-{
-	__put_css_set(cg, 0);
-}
-
-static inline void put_css_set_taskexit(struct css_set *cg)
-{
-	__put_css_set(cg, 1);
 }
 
 /*
@@ -1990,6 +1976,7 @@ int cgroup_attach_task(struct cgroup *cgrp, struct task_struct *tsk)
 			ss->attach(cgrp, &tset);
 	}
 
+	set_bit(CGRP_RELEASABLE, &cgrp->flags);
 	synchronize_rcu();
 
 	/*
@@ -4020,6 +4007,8 @@ static long cgroup_create(struct cgroup *parent, struct dentry *dentry,
 		if (!ss->__DEPRECATED_clear_css_refs)
 			dget(dentry);
 
+	set_bit(CGRP_RELEASABLE, &parent->flags);
+
 	/* The cgroup directory was pre-locked for us */
 	BUG_ON(!mutex_is_locked(&cgrp->dentry->d_inode->i_mutex));
 
@@ -4257,7 +4246,6 @@ again:
 	cgroup_d_remove_dir(d);
 	dput(d);
 
-	set_bit(CGRP_RELEASABLE, &parent->flags);
 	check_for_release(parent);
 
 	/*
@@ -4908,7 +4896,7 @@ void cgroup_exit(struct task_struct *tsk, int run_callbacks)
 	task_unlock(tsk);
 
 	if (cg)
-		put_css_set_taskexit(cg);
+		put_css_set(cg);
 }
 
 /**
@@ -4977,6 +4965,14 @@ bool __css_tryget(struct cgroup_subsys_state *css)
 EXPORT_SYMBOL_GPL(__css_tryget);
 
 /* Caller must verify that the css is not for root cgroup */
+void __css_get(struct cgroup_subsys_state *css, int count)
+{
+	atomic_add(count, &css->refcnt);
+	set_bit(CGRP_RELEASABLE, &css->cgroup->flags);
+}
+EXPORT_SYMBOL_GPL(__css_get);
+
+/* Caller must verify that the css is not for root cgroup */
 void __css_put(struct cgroup_subsys_state *css)
 {
 	struct cgroup *cgrp = css->cgroup;
@@ -4987,10 +4983,7 @@ void __css_put(struct cgroup_subsys_state *css)
 
 	switch (v) {
 	case 1:
-		if (notify_on_release(cgrp)) {
-			set_bit(CGRP_RELEASABLE, &cgrp->flags);
-			check_for_release(cgrp);
-		}
+		check_for_release(cgrp);
 		cgroup_wakeup_rmdir_waiter(cgrp);
 		break;
 	case 0:
