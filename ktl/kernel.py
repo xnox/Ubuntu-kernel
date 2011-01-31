e8ee15ccc6168167c69003c054cfde74d84e7dbd
Steve Conklin <sconklin@canonical.com>
2011-01-31 17:24:36 -0600

    new script to scrape rmadison, plus supporting module changes

--- ktl/kernel.py
+++ ktl/kernel.py
@@ -4,6 +4,7 @@
 from ktl.git                            import Git, GitError
 
 map_kernel_version_to_ubuntu_release = {
+    '2.6.38' : { 'number' : '11.04', 'name'  : 'natty', 'sha1' : '', 'md5' : '' },
     '2.6.35' : { 'number' : '10.10', 'name'  : 'maverick', 'sha1' : 'a2422f9281766ffe2f615903712819b9b0d9dd52', 'md5' : '62001687bd94d1c0dd9a3654c64257d6' },
     '2.6.32' : { 'number' : '10.04', 'name'  : 'lucid',    'sha1' : '298cbfdb55fc64d1135f06b3bed3c8748123c183', 'md5' : '4b1f6f6fac43a23e783079db589fc7e2' },
     '2.6.31' : { 'number' : '9.10',  'name'  : 'karmic',   'sha1' : '6b19c2987b0e2d74dcdca2aadebd5081bc143b72', 'md5' : '16c0355d3612806ef87addf7c9f8c9f9' },
@@ -11,7 +12,8 @@ map_kernel_version_to_ubuntu_release = {
     '2.6.27' : { 'number' : '8.10',  'name'  : 'intrepid', 'sha1' : '', 'md5' : '' },
     '2.6.24' : { 'number' : '8.04',  'name'  : 'hardy',    'sha1' : 'ccccdc4759fd780a028000a1b7b15dbd9c60363b', 'md5' : 'e4aad2f8c445505cbbfa92864f5941ab' },
     '2.6.22' : { 'number' : '7.10',  'name'  : 'gutsy',    'sha1' : '', 'md5' : '' },
-    '2.6.20' : { 'number' : '7.04',  'name'  : 'feisty',   'sha1' : '', 'md5' : '' }
+    '2.6.20' : { 'number' : '7.04',  'name'  : 'feisty',   'sha1' : '', 'md5' : '' },
+    '2.6.15' : { 'number' : '6.06',  'name'  : 'dapper',   'sha1' : '', 'md5' : '' }
 }
 
 map_release_number_to_ubuntu_release = {
@@ -24,6 +26,21 @@ map_release_number_to_ubuntu_release = {
     '7.10'   : { 'kernel' : '2.6.22', 'name' : 'gutsy'    },
     '7.04'   : { 'kernel' : '2.6.20', 'name' : 'feisty'   }
 }
+
+kernel_package_names = [
+    'linux',
+    'linux-ec2',
+    'linux-meta',
+    'linux-meta-ec2',
+    'linux-ports-meta',
+    'linux-source-2.6.15', # remove when dapper is dead
+    'linux-backports-modules-2.6.15',
+    'linux-backports-modules-2.6.32',
+    'linux-restricted-modules-2.6.24',
+    'linux-ubuntu-modules-2.6.24',
+    'linux-backports-modules-2.6.35',
+    'linux-lts-backport-maverick',
+]
 
 # KernelError
 #
