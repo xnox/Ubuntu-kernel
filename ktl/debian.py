94116bd51b2b27829ea4edc517ffefc8e147d9ec
Brad Figg <brad.figg@canonical.com>
2011-02-02 14:57:48 -0800

    Added regular expression which can be used by other code.
    Changed how the correct packages are identified by the sru-report.
    
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- ktl/debian.py
+++ ktl/debian.py
@@ -23,6 +23,7 @@ class DebianError(Exception):
 class Debian:
     debug = False
     version_line_rc = compile("^(linux[-\S]*) \(([0-9]+\.[0-9]+\.[0-9]+[-\.][0-9]+\.[0-9]+)\) (\S+); urgency=\S+$")
+    version_rc      = compile("^([0-9]+\.[0-9]+\.[0-9]+)[-\.]([0-9]+)\.([0-9]+)$")
 
     # raw_changelog
     #
