c7fb07ffd1b9f9ddd6820f04e82a6e423f966f04
Steve Conklin <sconklin@canonical.com>
2011-02-04 17:07:50 -0600

    more work on archive fetching

--- ktl/archive.py
+++ ktl/archive.py
@@ -28,6 +28,8 @@ class Archive:
     #file_lifetime = 900 # 15 minutes
     file_lifetime = 60 # 1 minute (for testing)
 
+    statuses = ['Pending', 'Published']
+
     # related to the kernel ppa
     __ppa_get_deleted       = False
     __ppa_get_obsolete      = False
@@ -38,13 +40,27 @@ class Archive:
     ppaname = 'ppa'
     ppafilename = 'ckt-ppa.json'
 
+    # related to the kernel distro archive
+    __distro_get_deleted       = False
+    __distro_get_obsolete      = False
+    __distro_get_superseded    = False
+    distro = None
+    alldistroinfo = False
+    distrofilename = 'distro-archive.json'
+
     # version
     #
     def ppa_versions(self, force = False):
         self.__fetch_ppa_if_needed(force)
         return self.ppa
 
-    # __fetch_if_needed
+    # version
+    #
+    def distro_versions(self, force = False):
+        self.__fetch_distro_if_needed(force)
+        return self.distro
+
+    # __fetch_ppa_if_needed
     #
     def __fetch_ppa_if_needed(self, force):
         # see if there is a local json file that is recent
@@ -54,11 +70,8 @@ class Archive:
         if force or (not age) or (age > self.file_lifetime):
             # fetch from the PPA
             print 'Fetching from Launchpad'
-            print 'force ', force
-            print 'age ', age
 
-            # by default we want these
-            statuses = ['Pending', 'Published']
+            statuses = list(self.statuses)
             if self.__ppa_get_deleted:
                 statuses.append('Deleted')
             if self.__ppa_get_obsolete:
@@ -69,8 +82,8 @@ class Archive:
             jf = open(self.ppafilename, 'w+')
 
             outdict = {}
-            launchpad = Launchpad.login_anonymously('kernel team tools', 'production', self.cachedir)
-            person = launchpad.people[self.teamname]
+            lp = Launchpad.login_anonymously('kernel team tools', 'production', self.cachedir)
+            person = lp.people[self.teamname]
             ppa = person.getPPAByName(name=self.ppaname)
 
             for astatus in statuses:
@@ -84,7 +97,7 @@ class Archive:
                     sourceinfo['signer'] = sourceinfo['package_signer_link'].split('/')[-1].strip('~') 
                     rm = re.match('[0-9]\.[0-9]\.[0-9][0-9]', sourceinfo['source_package_version'])
                     version = rm.group(0)
-                    sourceinfo['distribution'] = map_kernel_version_to_ubuntu_release[version]['name']
+                    sourceinfo['series'] = map_kernel_version_to_ubuntu_release[version]['name']
                     # And strip some things we don't care about
                     if not self.allppainfo:
                         for delkey in ['archive_link', 'distro_series_link', 'http_etag', 'package_maintainer_link', \
@@ -98,18 +111,99 @@ class Archive:
             jf.close()
 
             self.ppa = outdict
-
         else:
-
             # read from the local file
             f = open(self.ppafilename, 'r')
             # read it
             self.ppa = json.load(f)
             f.close()
-    
+        return
 
 
+    # __fetch_distro_if_needed
+    #
+    def __fetch_distro_if_needed(self, force):
+        # see if there is a local json file that is recent
+        # if there is, return the data
+        age = fileage(self.distrofilename)
 
+        if force or (not age) or (age > self.file_lifetime):
+            # fetch from the PPA
+            print 'Fetching from Distro archives'
+
+            statuses = list(self.statuses)
+            if self.__distro_get_deleted:
+                statuses.append('Deleted')
+            if self.__distro_get_obsolete:
+                statuses.append('Obsolete')
+            if self.__distro_get_superseded:
+                statuses.append('Superseded')
+
+            jf = open(self.ppafilename, 'w+')
+
+            lp = Launchpad.login_anonymously('kernel team tools', 'production', self.cachedir)
+            outdict = {}
+
+            # get a list of the series that we want info about
+            #for key, info in map_release_number_to_ubuntu_release.items():
+            #    if not info['supported']:
+            #        continue
+
+            #archive = lp.distributions['ubuntu'].getSeries(name_or_version=info['name']).main_archive
+            archive = lp.distributions['ubuntu'].getArchive(name='primary')
+
+            for astatus in statuses:
+                for pname in kernel_package_names:
+                    for key, release in map_release_number_to_ubuntu_release.items():
+                        if not release['supported']:
+                            if self.debug:
+                                print 'DEBUG: Fetching from archive, skipping release ', release['name']
+                            continue
+
+                        series = lp.distributions['ubuntu'].getSeries(name_or_version=release['name'])
+                        psrc = archive.getPublishedSources(status=astatus, exact_match = True, source_name = pname, distro_series = series)
+                        #psrc = archive.getPublishedSources(status=astatus, exact_match = True, source_name = pname)
+                        for p in  psrc:
+                            fd = urlopen(p.self_link)
+                            sourceinfo = json.load(fd)
+                            #print json.dumps(sourceinfo, sort_keys=True, indent=4)
+
+                            # Add some plain text fields for some info
+                            field = sourceinfo['package_creator_link']
+                            if field:
+                                sourceinfo['creator'] = field.split('/')[-1].strip('~') 
+                            else:
+                                sourceinfo['creator'] = 'Unknown'
+                            field = sourceinfo['package_signer_link']
+                            if field:
+                                sourceinfo['signer'] = field.split('/')[-1].strip('~') 
+                            else:
+                                sourceinfo['signer'] = 'Unknown'
+                            rm = re.match('[0-9]\.[0-9]\.[0-9][0-9]', sourceinfo['source_package_version'])
+                            version = rm.group(0)
+                            try:
+                                sourceinfo['series'] = map_kernel_version_to_ubuntu_release[version]['name']
+                            except:
+                                sourceinfo['series'] = 'Unknown'
+                            # And strip some things we don't care about
+                            if not self.allppainfo:
+                                for delkey in ['archive_link', 'distro_series_link', 'http_etag', 'package_maintainer_link', \
+                                                   'resource_type_link', 'package_creator_link', 'package_signer_link', \
+                                                   'section_name', 'scheduled_deletion_date', 'removal_comment', 'removed_by_link']:
+                                    del sourceinfo[delkey]
+
+                            key = p.source_package_name + '-' + p.source_package_version
+                            print '    found: ', key
+                            outdict[key] = sourceinfo
+            jf.write(json.dumps(outdict, sort_keys=True, indent=4))
+            jf.close()
+            self.ppa = outdict
+        else:
+            # read from the local file
+            f = open(self.ppafilename, 'r')
+            # read it
+            self.ppa = json.load(f)
+            f.close()
         return
 
 #lp.distributions['ubuntu'].getSeries(name_or_version='maverick').main_archive
