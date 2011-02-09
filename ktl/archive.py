716c051a81f7df16aa772fe2a0c1b971a8e2bcc5
Steve Conklin <sconklin@canonical.com>
2011-02-09 11:33:44 -0600

    Better caching and some error checking

--- ktl/archive.py
+++ ktl/archive.py
@@ -82,62 +82,61 @@ class Archive:
         # if there is, return the data
         age = fileage(self.ppafilename)
 
-        if force or (not age) or (age > self.file_lifetime):
-            # fetch from the PPA
-            print 'Fetching from Launchpad'
-
-            statuses = list(self.statuses)
-            if self.__ppa_get_deleted:
-                statuses.append('Deleted')
-            if self.__ppa_get_obsolete:
-                statuses.append('Obsolete')
-            if self.__ppa_get_superseded:
-                statuses.append('Superseded')
-
-            jf = open(self.ppafilename, 'w+')
-
-            outdict = {}
-            lp = Launchpad.login_anonymously('kernel team tools', 'production', self.cachedir)
-            person = lp.people[self.teamname]
-            ppa = person.getPPAByName(name=self.ppaname)
-
-            for astatus in statuses:
-                psrc = ppa.getPublishedSources(status=astatus)
-                for p in  psrc:
-                    fd = urlopen(p.self_link)
-                    sourceinfo = json.load(fd)
-
-                    # Add some plain text fields for some info
-                    # -spc-
-                    print json.dumps(sourceinfo, sort_keys=True, indent=4)
-                    sourceinfo['creator'] = sourceinfo['package_creator_link'].split('/')[-1].strip('~') 
-                    sourceinfo['signer'] = sourceinfo['package_signer_link'].split('/')[-1].strip('~') 
-                    rm = re.match('[0-9]\.[0-9]\.[0-9][0-9]', sourceinfo['source_package_version'])
-                    version = rm.group(0)
-                    # TODO fix this
-                    #sourceinfo['series'] = map_kernel_version_to_ubuntu_release[version]['name']
-                    sourceinfo['series'] = sourceinfo['display_name'].split()[-1]
-                    # And strip some things we don't care about
-                    if not self.allppainfo:
-                        for delkey in ['archive_link', 'distro_series_link', 'http_etag', 'package_maintainer_link', \
-                                           'resource_type_link', 'package_creator_link', 'package_signer_link', \
-                                           'section_name', 'scheduled_deletion_date', 'removal_comment', 'removed_by_link']:
-                            del sourceinfo[delkey]
-
-                    key = p.source_package_name + '-' + p.source_package_version
-                    outdict[key] = sourceinfo
-            jf.write(json.dumps(outdict, sort_keys=True, indent=4))
-            jf.close()
-
-            self.ppa = outdict
-        else:
-            # read from the local file
+        if (not force) and age and (age < self.file_lifetime):
+            # Try to fetch from the local file
             f = open(self.ppafilename, 'r')
-            # read it
             self.ppa = json.load(f)
             f.close() 
             if self.debug:
                 print 'Read cached PPA data, records:', len(self.ppa)
+            if len(self.ppa) != 0:
+                return
+            else:
+                print 'Got nothing for PPA from cached results, fetching from Launchpad'
+
+        # fetch from the PPA
+        print 'Fetching from Launchpad'
+
+        statuses = list(self.statuses)
+        if self.__ppa_get_deleted:
+            statuses.append('Deleted')
+        if self.__ppa_get_obsolete:
+            statuses.append('Obsolete')
+        if self.__ppa_get_superseded:
+            statuses.append('Superseded')
+
+        outdict = {}
+        lp = Launchpad.login_anonymously('kernel team tools', 'production', self.cachedir)
+        person = lp.people[self.teamname]
+        ppa = person.getPPAByName(name=self.ppaname)
+
+        for astatus in statuses:
+            psrc = ppa.getPublishedSources(status=astatus)
+            for p in  psrc:
+                fd = urlopen(p.self_link)
+                sourceinfo = json.load(fd)
+
+                # Add some plain text fields for some info
+                sourceinfo['creator'] = sourceinfo['package_creator_link'].split('/')[-1].strip('~') 
+                sourceinfo['signer'] = sourceinfo['package_signer_link'].split('/')[-1].strip('~') 
+                rm = re.match('[0-9]\.[0-9]\.[0-9][0-9]', sourceinfo['source_package_version'])
+                version = rm.group(0)
+                sourceinfo['series'] = sourceinfo['display_name'].split()[-1]
+                # And strip some things we don't care about
+                if not self.allppainfo:
+                    for delkey in ['archive_link', 'distro_series_link', 'http_etag', 'package_maintainer_link', \
+                                       'resource_type_link', 'package_creator_link', 'package_signer_link', \
+                                       'section_name', 'scheduled_deletion_date', 'removal_comment', 'removed_by_link']:
+                        del sourceinfo[delkey]
+
+                key = p.source_package_name + '-' + p.source_package_version
+                outdict[key] = sourceinfo
+
+        jf = open(self.ppafilename, 'w+')
+        jf.write(json.dumps(outdict, sort_keys=True, indent=4))
+        jf.close()
+
+        self.ppa = outdict
         return
 
 
@@ -148,129 +147,132 @@ class Archive:
         # if there is, return the data
         age = fileage(self.distrofilename)
 
-        if force or (not age) or (age > self.file_lifetime):
-            # fetch from the PPA
-            if self.debug:
-                print 'Fetching from Distro archives'
-
-            statuses = list(self.statuses)
-            if self.__distro_get_deleted:
-                statuses.append('Deleted')
-            if self.__distro_get_obsolete:
-                statuses.append('Obsolete')
-            if self.__distro_get_superseded:
-                statuses.append('Superseded')
-
-            jf = open(self.distrofilename, 'w+')
-
-            lp = Launchpad.login_anonymously('kernel team tools', 'production', self.cachedir)
-            masteroutdict = {}
-
-            #archive = lp.distributions['ubuntu'].getSeries(name_or_version=info['name']).main_archive
-            archive = lp.distributions['ubuntu'].getArchive(name='primary')
-
-            for astatus in statuses:
-                for pname in kernel_package_names:
-                    if self.debug:
-                        print 'fetching for package', pname, 'status', astatus
-                    outdict = {}
-                    psrc = archive.getPublishedSources(status=astatus, exact_match = True, source_name = pname)
-                    for p in  psrc:
-                        fd = urlopen(p.self_link)
-                        sourceinfo = json.load(fd)
-                        if self.debug:
-                            print 'fetched', sourceinfo['source_package_name'], sourceinfo['source_package_version']
-
-                        # Add some plain text fields for some info
-                        field = sourceinfo['package_creator_link']
-                        if field:
-                            sourceinfo['creator'] = field.split('/')[-1].strip('~') 
-                        else:
-                            sourceinfo['creator'] = 'Unknown'
-                        field = sourceinfo['package_signer_link']
-                        if field:
-                            sourceinfo['signer'] = field.split('/')[-1].strip('~') 
-                        else:
-                            sourceinfo['signer'] = 'Unknown'
-                        rm = re.match('[0-9]\.[0-9]\.[0-9][0-9]', sourceinfo['source_package_version'])
-                        version = rm.group(0)
-                        sourceinfo['series'] = sourceinfo['display_name'].split()[-1]
-                        # And strip some things we don't care about
-                        if not self.allppainfo:
-                            for delkey in ['archive_link', 'distro_series_link', 'http_etag', 'package_maintainer_link', \
-                                               'resource_type_link', 'package_creator_link', 'package_signer_link', \
-                                               'section_name', 'scheduled_deletion_date', 'removal_comment', 'removed_by_link']:
-                                del sourceinfo[delkey]
-
-                        key = p.source_package_name + '-' + p.source_package_version + '-' + p.pocket
-                        if self.debug:
-                            print '    found: ', key
-                        outdict[key] = sourceinfo
-
-                    if len(outdict) == 0:
-                        if self.debug:
-                            print 'Nothing from ', astatus, pname
-                        continue
-
-                    #
-                    # Now we have all the data for this package name and status
-                    # Remove all the unsupported ones
-                    if self.debug:
-                        print 'records in outdict after fetch', len(outdict)
-                    unsupported = []
-                    serieslist = []
-                    for key, release in map_release_number_to_ubuntu_release.items():
-                        if not release['supported']:
-                            if self.debug:
-                                print 'DEBUG: Fetching from archive, will skip release ', release['name']
-                            unsupported.append(release['name'])
-                        else:
-                            serieslist.append(release['name'])
-                    # remove unwanted ones
-                    for name, sourceinfo in outdict.items():
-                        if sourceinfo['series'] in unsupported:
-                            if self.debug:
-                                print 'DEBUG: Fetching from archive, skipping ', name
-                            del(outdict[name])
-
-                    #
-                    # We now have a collection of all supported packages for a given
-                    # package name and status. Within each permutation of series and
-                    # pocket, keep only the highest version
-                    for series in serieslist:
-                        for pocket in pocket_list:
-                            templist = {}
-                            for name, sourceinfo in outdict.items():
-                                if sourceinfo['pocket'] == pocket and sourceinfo['series'] == series:
-                                    if self.debug:
-                                        print 'found matching',  json.dumps(sourceinfo, sort_keys=True, indent=4)
-                                    templist[sourceinfo['source_package_version']] = name
-                            # Now sort the templist
-                            slist = sorted(templist, compare_versions, reverse=True)
-                            # and delete all but the highest version from the main list
-                            for k in range(1, len(slist)):
-                                if self.debug:
-                                    print 'deleting', templist[slist[k]]
-                                # If the same version was in Security and Updates . . .
-                                del(outdict[templist[slist[k]]])
-
-                    if self.debug:
-                        print "Updating Master"
-                        print 'records in outdict', len(outdict)
-                        print 'records in masteroutdict', len(masteroutdict)
-                        #print json.dumps(outdict, sort_keys=True, indent=4)
-                    masteroutdict.update(outdict)
-
-            jf.write(json.dumps(masteroutdict, sort_keys=True, indent=4))
-            jf.close()
-            self.distro = masteroutdict
-        else:
+        if (not force) and age and (age < self.file_lifetime):
             # read from the local file
             f = open(self.distrofilename, 'r')
             self.distro = json.load(f)
             f.close()
             if self.debug:
-                print 'Read cached PPA data, records:', len(self.distro)
+                print 'Read cached Distro data, records:', len(self.distro)
+            if len(self.distro) != 0:
+                return
+            else:
+                print 'Got nothing for distro from cached results, fetching from Launchpad'
+
+        # fetch from the PPA
+        if self.debug:
+            print 'Fetching from Distro archives'
+
+        statuses = list(self.statuses)
+        if self.__distro_get_deleted:
+            statuses.append('Deleted')
+        if self.__distro_get_obsolete:
+            statuses.append('Obsolete')
+        if self.__distro_get_superseded:
+            statuses.append('Superseded')
+
+        lp = Launchpad.login_anonymously('kernel team tools', 'production', self.cachedir)
+        masteroutdict = {}
+
+        #archive = lp.distributions['ubuntu'].getSeries(name_or_version=info['name']).main_archive
+        archive = lp.distributions['ubuntu'].getArchive(name='primary')
+
+        for astatus in statuses:
+            for pname in kernel_package_names:
+                if self.debug:
+                    print 'fetching for package', pname, 'status', astatus
+                outdict = {}
+                psrc = archive.getPublishedSources(status=astatus, exact_match = True, source_name = pname)
+                for p in  psrc:
+                    fd = urlopen(p.self_link)
+                    sourceinfo = json.load(fd)
+                    if self.debug:
+                        print 'fetched', sourceinfo['source_package_name'], sourceinfo['source_package_version']
+
+                    # Add some plain text fields for some info
+                    field = sourceinfo['package_creator_link']
+                    if field:
+                        sourceinfo['creator'] = field.split('/')[-1].strip('~') 
+                    else:
+                        sourceinfo['creator'] = 'Unknown'
+                    field = sourceinfo['package_signer_link']
+                    if field:
+                        sourceinfo['signer'] = field.split('/')[-1].strip('~') 
+                    else:
+                        sourceinfo['signer'] = 'Unknown'
+                    rm = re.match('[0-9]\.[0-9]\.[0-9][0-9]', sourceinfo['source_package_version'])
+                    version = rm.group(0)
+                    sourceinfo['series'] = sourceinfo['display_name'].split()[-1]
+                    # And strip some things we don't care about
+                    if not self.allppainfo:
+                        for delkey in ['archive_link', 'distro_series_link', 'http_etag', 'package_maintainer_link', \
+                                           'resource_type_link', 'package_creator_link', 'package_signer_link', \
+                                           'section_name', 'scheduled_deletion_date', 'removal_comment', 'removed_by_link']:
+                            del sourceinfo[delkey]
+
+                    key = p.source_package_name + '-' + p.source_package_version + '-' + p.pocket
+                    if self.debug:
+                        print '    found: ', key
+                    outdict[key] = sourceinfo
+
+                if len(outdict) == 0:
+                    if self.debug:
+                        print 'Nothing from ', astatus, pname
+                    continue
+
+                #
+                # Now we have all the data for this package name and status
+                # Remove all the unsupported ones
+                if self.debug:
+                    print 'records in outdict after fetch', len(outdict)
+                unsupported = []
+                serieslist = []
+                for key, release in map_release_number_to_ubuntu_release.items():
+                    if not release['supported']:
+                        if self.debug:
+                            print 'DEBUG: Fetching from archive, will skip release ', release['name']
+                        unsupported.append(release['name'])
+                    else:
+                        serieslist.append(release['name'])
+                # remove unwanted ones
+                for name, sourceinfo in outdict.items():
+                    if sourceinfo['series'] in unsupported:
+                        if self.debug:
+                            print 'DEBUG: Fetching from archive, skipping ', name
+                        del(outdict[name])
+
+                #
+                # We now have a collection of all supported packages for a given
+                # package name and status. Within each permutation of series and
+                # pocket, keep only the highest version
+                for series in serieslist:
+                    for pocket in pocket_list:
+                        templist = {}
+                        for name, sourceinfo in outdict.items():
+                            if sourceinfo['pocket'] == pocket and sourceinfo['series'] == series:
+                                if self.debug:
+                                    print 'found matching',  json.dumps(sourceinfo, sort_keys=True, indent=4)
+                                templist[sourceinfo['source_package_version']] = name
+                        # Now sort the templist
+                        slist = sorted(templist, compare_versions, reverse=True)
+                        # and delete all but the highest version from the main list
+                        for k in range(1, len(slist)):
+                            if self.debug:
+                                print 'deleting', templist[slist[k]]
+                            # If the same version was in Security and Updates . . .
+                            del(outdict[templist[slist[k]]])
+
+                if self.debug:
+                    print "Updating Master"
+                    print 'records in outdict', len(outdict)
+                    print 'records in masteroutdict', len(masteroutdict)
+                    #print json.dumps(outdict, sort_keys=True, indent=4)
+                masteroutdict.update(outdict)
+
+        jf = open(self.distrofilename, 'w+')
+        jf.write(json.dumps(masteroutdict, sort_keys=True, indent=4))
+        jf.close()
+        self.distro = masteroutdict
         return
 
 # vi:set ts=4 sw=4 expandtab:
