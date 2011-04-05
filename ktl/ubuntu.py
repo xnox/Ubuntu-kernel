5933c9f50fc52b857448e390118839f9fc7297ab
Brad Figg <brad.figg@canonical.com>
2011-04-05 12:49:56 -0700

    A reworked database of all the information for the different series.
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

---
+++ ktl/ubuntu.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python
+#
+
+#
+# Warning - using the following dictionary to get the series name from the kernel version works for the linux package,
+# but not for some others (some ARM packages are known to be wrong). This is because the ARM kernels used for some
+# series are not the same as the main kernels
+#
+
+# UbuntuError
+#
+class UbuntuError(Exception):
+    # __init__
+    #
+    def __init__(self, error):
+        self.msg = error
+
+# Ubuntu
+#
+class Ubuntu:
+
+    db = {
+        '11.10' :
+        {
+            'series_version' : '11.10',
+            'kernel'    : '2.6.39',
+            'name'      : 'oneiric',
+            'supported' : False,
+            # adjust packages when this goes live
+            'packages'  :
+            [
+                'linux',
+            ]
+        },
+        '11.04' :
+        {
+            'series_version' : '11.04',
+            'kernel'    : '2.6.38',
+            'name'      : 'natty',
+            'supported' : True,
+            # adjust packages when this goes live
+            'packages'  :
+            [
+                'linux',
+                'linux-ti-omap4',
+                'linux-mvl-dove',
+                'linux-ec2',
+                'linux-meta',
+                'linux-ports-meta',
+                'linux-meta-ec2',
+                'linux-meta-mvl-dove',
+                'linux-meta-ti-omap4'
+            ]
+        },
+        '10.10' :
+        {
+            'series_version' : '10.10',
+            'kernel' : '2.6.35',
+            'name' : 'maverick',
+            'supported' : True,
+            'packages' :
+            [
+                'linux',
+                'linux-ti-omap4',
+                'linux-mvl-dove',
+                'linux-meta',
+                'linux-ports-meta',
+                'linux-meta-mvl-dove',
+                'linux-meta-ti-omap4',
+                'linux-backports-modules-2.6.35'
+            ],
+            'sha1' : 'a2422f9281766ffe2f615903712819b9b0d9dd52',
+            'md5' : '62001687bd94d1c0dd9a3654c64257d6'
+        },
+        '10.04' :
+        {
+            'series_version' : '10.04',
+            'kernel' : '2.6.32',
+            'name' : 'lucid',
+            'supported' : True,
+            'packages' :
+            [
+                'linux',
+                'linux-fsl-imx51',
+                'linux-mvl-dove',
+                'linux-ec2',
+                'linux-meta',
+                'linux-ports-meta',
+                'linux-meta-ec2',
+                'linux-meta-mvl-dove',
+                'linux-meta-fsl-imx51',
+                'linux-backports-modules-2.6.32',
+                'linux-lts-backport-maverick',
+                'linux-meta-lts-backport-maverick' #, 'linux-lts-backport-natty'
+            ],
+            'sha1' : '298cbfdb55fc64d1135f06b3bed3c8748123c183',
+            'md5' : '4b1f6f6fac43a23e783079db589fc7e2'
+        },
+        '9.10' :
+        {
+            'series_version' : '9.10',
+            'kernel' : '2.6.31',
+            'name' : 'karmic',
+            'supported' : True,
+            'packages' :
+            [
+                'linux',
+                'linux-fsl-imx51',
+                'linux-mvl-dove',
+                'linux-ec2',
+                'linux-meta',
+                'linux-ports-meta',
+                'linux-meta-ec2',
+                'linux-meta-mvl-dove',
+                'linux-meta-fsl-imx51',
+                'linux-backports-modules-2.6.31'
+            ],
+            'sha1' : '6b19c2987b0e2d74dcdca2aadebd5081bc143b72',
+            'md5' : '16c0355d3612806ef87addf7c9f8c9f9'
+        },
+        '9.04' :
+        {
+            'series_version' : '9.04',
+            'kernel' : '2.6.28',
+            'name' : 'jaunty',
+            'supported' : False,
+            'packages' : [],
+            'sha1' : '92d6a293200566646fbb9215e0633b4b9312ad38',
+            'md5' : '062c29b626a55f09a65532538a6184d4'
+        },
+        '8.10' :
+        {
+            'series_version' : '8.10',
+            'kernel' : '2.6.27',
+            'name' : 'intrepid',
+            'supported' : False,
+            'packages' : []
+        },
+        '8.04' :
+        {
+            'series_version' : '8.04',
+            'kernel' : '2.6.24',
+            'name' : 'hardy',
+            'supported' : True,
+            'packages' :
+            [
+                'linux',
+                'linux-meta',
+                'linux-backports-modules-2.6.24',
+                'linux-ubuntu-modules-2.6.24',
+                'linux-restricted-modules-2.6.24'
+            ],
+            'sha1' : 'ccccdc4759fd780a028000a1b7b15dbd9c60363b',
+            'md5' : 'e4aad2f8c445505cbbfa92864f5941ab'
+        },
+        '7.10' :
+        {
+            'series_version' : '7.10',
+            'kernel' : '2.6.22',
+            'name' : 'gutsy',
+            'supported' : False,
+            'packages' : []
+        },
+        '7.04' :
+        {
+            'series_version' : '7.04',
+            'kernel' : '2.6.20',
+            'name' : 'feisty',
+            'supported' : False,
+            'packages' : []
+        },
+        '6.06' :
+        {
+            'series_version' : '6.06',
+            'kernel' : '2.6.15',
+            'name' : 'dapper',
+            'supported' : True,
+            'packages' :
+            [
+                'linux-source-2.6.15',
+                'linux-backports-modules-2.6.15',
+                'linux-restricted-modules-2.6.15'
+            ]
+        },
+    }
+
+    index_by_kernel_version = {
+        '2.6.38'   : db['11.04'],
+        '2.6.35'   : db['10.10'],
+        '2.6.32'   : db['10.04'],
+        '2.6.31'   : db['9.10'],
+        '2.6.28'   : db['9.04'],
+        '2.6.27'   : db['8.10'],
+        '2.6.24'   : db['8.04'],
+        '2.6.22'   : db['7.10'],
+        '2.6.20'   : db['7.04'],
+        '2.6.15'   : db['6.06'],
+    }
+
+    index_by_series_name = {
+        'natty'    : db['11.04'],
+        'maverick' : db['10.10'],
+        'lucid'    : db['10.04'],
+        'karmic'   : db['9.10'],
+        'jaunty'   : db['9.04'],
+        'intrepid' : db['8.10'],
+        'hardy'    : db['8.04'],
+        'gutsy'    : db['7.10'],
+        'feisty'   : db['7.04'],
+        'dapper'   : db['6.06'],
+    }
+
+    kernel_source_packages = [
+        'linux',
+        'linux-ti-omap4', # maverick, natty
+        'linux-mvl-dove', # maverick, karmic, lucid
+        'linux-fsl-imx51', # karmic, lucid
+        'linux-ec2',
+        'linux-meta',
+        'linux-meta-ec2',
+        'linux-meta-mvl-dove', # maverick, karmic, lucid
+        'linux-meta-ti-omap4', # maverick, natty
+        'linux-meta-fsl-imx51', # karmic, lucid ?
+        'linux-ports-meta',
+        'linux-source-2.6.15',
+
+        'linux-backports-modules-2.6.15',
+        'linux-backports-modules-2.6.24',
+        'linux-backports-modules-2.6.31',
+        'linux-backports-modules-2.6.32',
+        'linux-backports-modules-2.6.35',
+
+        'linux-restricted-modules-2.6.15',
+        'linux-restricted-modules-2.6.24',
+
+        'linux-ubuntu-modules-2.6.24',
+
+        'linux-lts-backport-maverick',
+        'linux-lts-backport-natty',
+        'linux-meta-lts-backport-maverick',
+        'linux-meta-lts-backport-natty',
+    ]
+
+    # lookup
+    #
+    def lookup(self, key):
+        if key in Ubuntu.db:
+            record = Ubuntu.db[key]
+        elif key in Ubuntu.index_by_kernel_version:
+            record = Ubuntu.index_by_kernel_version[key]
+        elif key in Ubuntu.index_by_series_name:
+            record = Ubuntu.index_by_series_name[key]
+        else:
+            raise KeyError(key)
+
+        return record
+
+if __name__ == '__main__':
+    ubuntu = Ubuntu()
+    db = ubuntu.db
+
+    #for record in db:
+    #    print db[record]
+
+    print(ubuntu.lookup('2.6.32'))
+    print(ubuntu.lookup('lucid'))
+    print(ubuntu.lookup('10.04'))
+
+# vi:set ts=4 sw=4 expandtab:
