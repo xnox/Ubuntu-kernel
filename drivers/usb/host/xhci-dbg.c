de9d0988d709055f679871ac23d536b06075783c
Maarten Lankhorst <m.b.lankhorst@gmail.com>
2011-06-01 23:27:49 +0200

    xhci: Add defines for hardcoded slot states
    BugLink: http://bugs.launchpad.net/bugs/802541
    
    This needs to be added to the stable trees back to 2.6.34 to support an
    upcoming bug fix.
    
    Signed-off-by: Maarten Lankhorst <m.b.lankhorst@gmail.com>
    Signed-off-by: Sarah Sharp <sarah.a.sharp@linux.intel.com>
    Cc: stable@kernel.org
    (backported from commit e2b0217715c6d10379d94bdfe5560af96eecbb7c)
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/usb/host/xhci-dbg.c
+++ drivers/usb/host/xhci-dbg.c
@@ -436,14 +436,14 @@ char *xhci_get_slot_state(struct xhci_hcd *xhci,
 {
 	struct xhci_slot_ctx *slot_ctx = xhci_get_slot_ctx(xhci, ctx);
 
-	switch (GET_SLOT_STATE(slot_ctx->dev_state)) {
-	case 0:
+	switch (GET_SLOT_STATE(le32_to_cpu(slot_ctx->dev_state))) {
+	case SLOT_STATE_ENABLED:
 		return "enabled/disabled";
-	case 1:
+	case SLOT_STATE_DEFAULT:
 		return "default";
-	case 2:
+	case SLOT_STATE_ADDRESSED:
 		return "addressed";
-	case 3:
+	case SLOT_STATE_CONFIGURED:
 		return "configured";
 	default:
 		return "reserved";
