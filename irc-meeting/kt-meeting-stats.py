98f0c7d2d61ffdf2b8aaf19914a2117faeee8885
Leann Ogasawara <leann.ogasawara@canonical.com>
2011-06-03 12:48:17 -0700

    irc-meeting/kt-meeting-stats.py: Add script to generate bug stats for weekly meeting
    The weekly kernel IRC meeting on freenode in #ubuntu-kernel posts a
    series of bug statistics relating to the devel and stable Ubuntu kernel
    releases.  This script generates the statistics that can be copied and
    posted into each week's meeting.
    
    Signed-off-by: Leann Ogasawara <leann.ogasawara@canonical.com>

---
+++ irc-meeting/kt-meeting-stats.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+# Generate the weekly bug stats for the Ubuntu Kernel Team IRC meeting.
+
+# This should be run within the kernel arsenal tree:
+#
+#   bzr branch lp:~canonical-kernel-team/arsenal/kernel kernel_arsenal
+#   cp kt-meeting-stats.py kernel_arsenal/contrib/linux/
+#   python ./kt-meeting-stats.py
+#
+# It is assumed this will be run in a cron _once a week_.  The script
+# takes approximately 45sec to run.
+#
+# NOTE: the stats are written to $filename and used as input upon the next
+# run of this script.  If you run this more frequently than once a week,
+# you'll get incorrect rate of change values for the week.
+
+from arsenal_lib import *
+import urllib
+
+#calculate the change in numbers from last week
+def calculate_change(stat, key):
+    if stat > stats[key]:
+        change = "up"
+        change_num = stat - stats[key]
+    elif stat < stats[key]:
+        change = "down"
+        change_num = stats[key] - stat
+    else:
+        change = "no change"
+        change_num = 0
+
+    return (change, change_num)
+
+stats = {}
+
+filename = "kt-meeting-stats.txt"
+if not os.path.exists(filename):
+    urllib.urlretrieve("http://reports.qa.ubuntu.com/reports/ogasawara/%s" %(filename), filename)
+
+# Import last week's stats
+FILE = open(filename, 'r')
+for line in FILE:
+    line = line.strip()
+    key_val = line.split(':')
+    key = key_val[0]
+    stats[key] = int(key_val[1])
+FILE.close()
+
+releases = ['oneiric', 'natty', 'maverick', 'lucid', 'hardy']
+regressions = ['regression-update', 'regression-release', 'regression-proposed']
+output = {'release':'',
+          'milestone':'',
+          'incoming':'',
+          'regression-update':'',
+          'regression-release':'',
+          'regression-proposed': ''}
+
+arsenal = Arsenal()
+ubuntu = arsenal.load_project('ubuntu')
+linux = ubuntu.getSourcePackage(name='linux')
+series = ubuntu.current_series
+linux_series = series.getSourcePackage(name='linux')
+
+# Release Metrics (nominated bugs)
+# LP:320596 - Series.searchTasks() always returns an empty collection
+# Should be fixed in later versions to not require omit_targeted=False
+devel_tasks = linux_series.searchTasks(omit_targeted=False)
+# LP:274074 Missing total_size on collections returned by named operations
+# Should be fixed in later version, so we could do len(tasks)
+nominated = devel_tasks._wadl_resource.representation['total_size']
+(change, change_num) = calculate_change(nominated, "nominated")
+output['release'] += "=== %s nominated bugs ===\n" % (series.name)
+output['release'] += " * %d linux kernel bugs (%s %d)\n" % (nominated, change, change_num)
+
+# Release Metrics (Ubuntu <current_milestone> bugs)
+size = len(series.active_milestones)
+milestone = series.active_milestones[size - 1]
+linux_milestone = linux.searchTasks(milestone=milestone)
+milestoned = linux_milestone._wadl_resource.representation['total_size']
+(change, change_num) = calculate_change(milestoned, "milestoned")
+output['release'] += "=== %s bugs ===\n" % (milestone.title)
+output['release'] += " * %s linux kernel bugs (%s %d)\n" % (milestoned, change, change_num)
+
+for release_name in releases:
+    # Release Metrics (<release>-updates bugs)
+    milestone_name = "%s-updates" % (release_name)
+    milestone = ubuntu.getMilestone(name=milestone_name)
+    release = linux.searchTasks(milestone=milestone)
+    update_bugs = release._wadl_resource.representation['total_size']
+    (change, change_num) = calculate_change(update_bugs, "%s-updates" % (release_name))
+    stats[milestone_name] = update_bugs
+    output['milestone'] += "=== %s-updates bugs ===\n" % (release_name)
+    output['milestone'] += " * %d linux kernel bugs (%s %d)\n" % (update_bugs, change, change_num)
+
+    # Incoming Bugs
+    release_bugs = linux.searchTasks(tags=release_name)
+    num_release_bugs = release_bugs._wadl_resource.representation['total_size']
+    stat_name = "%s-incoming" % (release_name)
+    (change, change_num) = calculate_change(num_release_bugs, stat_name)
+    stats[stat_name] = num_release_bugs
+    output['incoming'] += " * %d %s bugs (%s %d)\n" % (num_release_bugs, release_name, change, change_num)
+
+    # Regressions
+    for regression in regressions:
+        regression_bugs = linux.searchTasks(tags=[release_name,regression], tags_combinator="All")
+        num_regressions = regression_bugs._wadl_resource.representation['total_size']
+        stat_name = "%s-%s" % (release_name, regression)
+        (change, change_num) = calculate_change(num_regressions, stat_name)
+        stats[stat_name] = num_regressions
+        output[regression] += " * %d %s bugs (%s %d)\n" % (num_regressions, release_name, change, change_num)
+
+print "== Release Metrics =="
+print output['release']
+print output['milestone']
+print "== Incoming Bugs =="
+print output['incoming']
+print "== Regressions =="
+for regression in regressions:
+    print "=== %s bugs ===" % (regression)
+    print output[regression]
+
+# Save the meeting stats to use next week
+FILE = open(filename, 'w')
+for key in stats.keys():
+    FILE.write("%s:%s\n" % (key, stats[key]))
+FILE.close()
+    
+# vi:set ts=4 sw=4 expandtab:
