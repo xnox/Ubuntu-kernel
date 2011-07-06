fde2f08f3ec3823d363c4091e4337bbf9c2bf33c
Herton Ronaldo Krzesinski <herton.krzesinski@canonical.com>
2011-07-06 14:31:09 -0300

    ktl/ubuntu.py: add new fields for SRU Workflow use
    For each distro series, add optional 'dependent-packages',
    'derivative-packages', 'backport-packages' dictionary entries, which
    can be used later by SRU Workflow tools.
    
    Signed-off-by: Herton Ronaldo Krzesinski <herton.krzesinski@canonical.com>

--- ktl/ubuntu.py
+++ ktl/ubuntu.py
@@ -34,6 +34,15 @@ class Ubuntu:
                 'linux-ti-omap4',
                 'linux-meta-ti-omap4'
             ],
+            'dependent-packages' :
+            {
+                'linux' : [ 'linux-meta' ],
+                'linux-ti-omap4' : [ 'linux-meta-ti-omap4' ],
+            },
+            'derivative-packages' :
+            {
+                'linux' : [ 'linux-ti-omap4' ]
+            },
             'sha1' : '',
             'md5' : ''
         },
@@ -51,6 +60,15 @@ class Ubuntu:
                 'linux-meta-ti-omap4',
                 'linux-backports-modules-2.6.38'
             ],
+            'dependent-packages' :
+            {
+                'linux' : [ 'linux-meta', 'linux-backports-modules-2.6.38' ],
+                'linux-ti-omap4' : [ 'linux-meta-ti-omap4' ],
+            },
+            'derivative-packages' :
+            {
+                'linux' : [ 'linux-ti-omap4' ]
+            },
             'sha1' : '0770b9d2483eaeee4b80aec1fd448586b882003e',
             'md5' : 'cf0b587742611328f095da4b329e9fc7'
         },
@@ -71,6 +89,16 @@ class Ubuntu:
                 'linux-meta-ti-omap4',
                 'linux-backports-modules-2.6.35'
             ],
+            'dependent-packages' :
+            {
+                'linux' : [ 'linux-meta', 'linux-ports-meta', 'linux-backports-modules-2.6.35' ],
+                'linux-ti-omap4' : [ 'linux-meta-ti-omap4' ],
+                'linux-mvl-dove' : [ 'linux-meta-mvl-dove' ]
+            },
+            'derivative-packages' :
+            {
+                'linux' : [ 'linux-ti-omap4', 'linux-mvl-dove' ]
+            },
             'sha1' : 'a2422f9281766ffe2f615903712819b9b0d9dd52',
             'md5' : '62001687bd94d1c0dd9a3654c64257d6'
         },
@@ -97,6 +125,24 @@ class Ubuntu:
                 'linux-lts-backport-maverick',
                 'linux-meta-lts-backport-maverick' #, 'linux-lts-backport-natty'
             ],
+            'dependent-packages' :
+            {
+                'linux' : [ 'linux-meta', 'linux-ports-meta', 'linux-backports-modules-2.6.32' ],
+                'linux-fsl-imx51' : [ 'linux-meta-fsl-imx51' ],
+                'linux-mvl-dove' : [ 'linux-meta-mvl-dove' ],
+                'linux-ec2' : [ 'linux-meta-ec2' ],
+                'linux-lts-backport-natty' : [ 'linux-meta-lts-backport-natty' ],
+                'linux-lts-backport-maverick' : [ 'linux-meta-lts-backport-maverick' ]
+            },
+            'derivative-packages' :
+            {
+                'linux' : [ 'linux-fsl-imx51', 'linux-mvl-dove', 'linux-ec2' ]
+            },
+            'backport-packages' :
+            {
+                'linux-lts-backport-natty' : [ 'linux', '11.04' ],
+                'linux-lts-backport-natty' : [ 'linux', '10.10' ],
+            },
             'sha1' : '298cbfdb55fc64d1135f06b3bed3c8748123c183',
             'md5' : '4b1f6f6fac43a23e783079db589fc7e2'
         },
