41aaf8243c37ad4309a85754e1000954fcc0eb33
Brad Figg <brad.figg@canonical.com>
2011-08-15 05:14:29 -0700

    new_hands_off: identifies which bugs to not automatically process with the script
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

---
+++ bugs/bug_handlers/new_hands_off.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#
+
+from core.bug_handler                   import BugHandler
+
+class NewHandsOff(BugHandler):
+    """
+    """
+
+    # __init__
+    #
+    def __init__(self, lp, vout):
+        BugHandler.__init__(self, lp, vout)
+
+    # run
+    #
+    def run(self, bug, task, package_name):
+        """
+        There are certain bugs that we shouldn't mess with. This method tries to
+        know about those and returns True if they are supposed to be ignored.
+        """
+        retval = True
+
+        while True:
+
+            # A bug that is filed for a particular CVE. This is part of the CVE/SRU
+            # process.
+            #
+            if 'kernel-cve-tracking-bug' in bug.tags:
+                retval = False
+                break
+
+            # A bug that is used for workflow processes and is part of the kernel
+            # stable teams workflow.
+            #
+            if 'kernel-release-tracking-bug' in bug.tags:
+                retval = False
+                break
+
+            # The kernel stable team adds this tag onto bugs that get filed as part
+            # of a commit revert. It's so the team doesn't forget about the revert.
+            #
+            if ('stable-next' in bug.tags) or ('kernel-stable-next' in bug.tags):
+                retval = False
+                break
+
+            # As you'd expect, we shouldn't be touching private bugs.
+            #
+            if bug.private:
+                retval = False
+                break
+
+            # We should not be trying to change the status of "bug watch" tasks.
+            #
+            watch_link = task.bug_watch
+            if watch_link is not None:
+                retval = False
+                break
+
+            break;
+        return retval
+
+# vi:set ts=4 sw=4 expandtab:
