ac565c5db8c44971beb6a9654e9c03d23c8edaa2
Santosh Shilimkar <santosh.shilimkar@ti.com>
2011-09-11 21:10:04 +0530

    ARM: OMAP5: PM: Add the WakeupGen IP updates.
    OMAP4 and OMAP5 share same WakeupGen IP with below few udpates on OMAP5.
    - Additional 32 interrupt support is added w.r.t OMAP4 design.
    - The AUX CORE boot registers are now made accessible from non-secure SW.
    - SAR offset are changed and PTMSYNC* registers are removed from SAR.
    
    Patch updates the WakeupGen code accordingly.
    
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- arch/arm/mach-omap2/include/mach/omap-wakeupgen.h
+++ arch/arm/mach-omap2/include/mach/omap-wakeupgen.h
@@ -16,10 +16,12 @@
 #define OMAP_WKG_ENB_B_0			0x14
 #define OMAP_WKG_ENB_C_0			0x18
 #define OMAP_WKG_ENB_D_0			0x1c
+#define OMAP_WKG_ENB_E_0			0x20
 #define OMAP_WKG_ENB_A_1			0x410
 #define OMAP_WKG_ENB_B_1			0x414
 #define OMAP_WKG_ENB_C_1			0x418
 #define OMAP_WKG_ENB_D_1			0x41c
+#define OMAP_WKG_ENB_E_1			0x420
 #define OMAP_AUX_CORE_BOOT_0			0x800
 #define OMAP_AUX_CORE_BOOT_1			0x804
 #define OMAP_PTMSYNCREQ_MASK			0xc00
@@ -28,4 +30,5 @@
 #define OMAP_TIMESTAMPCYCLEHI			0xc0c
 
 extern int __init omap_wakeupgen_init(void);
+extern void __iomem *omap_get_wakeupgen_base(void);
 #endif
