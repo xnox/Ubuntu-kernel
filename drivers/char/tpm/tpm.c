375b9f3f41db4a4d9d606471fc21600e35274e01
Peter Huewe <huewe.external.infineon@googlemail.com>
2011-12-05 16:18:40 +0000

    TPM: Zero buffer after copying to userspace, CVE-2011-1162
    Since the buffer might contain security related data it might be a good idea to
    zero the buffer after we have copied it to userspace.
    
    This got assigned CVE-2011-1162.
    
    Signed-off-by: Rajiv Andrade <srajiv@linux.vnet.ibm.com>
    Cc: Stable Kernel <stable@kernel.org>
    Signed-off-by: James Morris <jmorris@namei.org>
    
    (backported from commit 3321c07ae5068568cd61ac9f4ba749006a7185c9)
    CVE-2011-1162
    BugLink: http://bugs.launchpad.net/bugs/899463
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Acked-by: Seth Forshee <seth.forshee@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- drivers/char/tpm/tpm.c
+++ drivers/char/tpm/tpm.c
@@ -997,6 +997,7 @@ ssize_t tpm_read(struct file *file, char __user *buf,
 {
 	struct tpm_chip *chip = file->private_data;
 	int ret_size;
+	int rc;
 
 	del_singleshot_timer_sync(&chip->user_read_timer);
 	flush_scheduled_work();
@@ -1007,8 +1008,11 @@ ssize_t tpm_read(struct file *file, char __user *buf,
 			ret_size = size;
 
 		mutex_lock(&chip->buffer_mutex);
-		if (copy_to_user(buf, chip->data_buffer, ret_size))
+		rc = copy_to_user(buf, chip->data_buffer, ret_size);
+		memset(chip->data_buffer, 0, ret_size);
+		if (rc)
 			ret_size = -EFAULT;
+
 		mutex_unlock(&chip->buffer_mutex);
 	}
 
