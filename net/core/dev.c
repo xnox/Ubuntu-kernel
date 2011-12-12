bfd18e5357e4db27f5332e901c304deea0f86f36
Stefan Bader <stefan.bader@canonical.com>
2011-12-12 15:55:15 +0100

    Revert "core: Fix memory leak/corruption on VLAN GRO_DROP, CVE-2011-1576"
    This reverts commit c636ae9cca8caff01c14cad37f8780da29fb2601 as
    there was a newer, more in the spirit of upstream fix for it.
    
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2602,8 +2602,7 @@ int napi_frags_finish(struct napi_struct *napi, struct sk_buff *skb, int ret)
 
 	case GRO_DROP:
 		err = NET_RX_DROP;
-		kfree_skb(skb);
-		break;
+		/* fall through */
 
 	case GRO_MERGED_FREE:
 		napi_reuse_skb(napi, skb);
