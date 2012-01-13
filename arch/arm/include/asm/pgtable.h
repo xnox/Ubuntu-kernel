cae1615bc6cc9c7fac75dc60ff1c930f27fe5239
Rob Herring <rob.herring@calxeda.com>
2012-01-13 10:24:33 +0100

    topdown mmap support
    Similar to other architectures, this adds topdown mmap support in user
    process address space allocation policy. This allows mmap sizes greater
    than 2GB. This support is largely copied from MIPS and the generic
    implementations.
    
    The address space randomization is moved into arch_pick_mmap_layout.
    
    Tested on V-Express with ubuntu and a mmap test from here:
    https://bugs.launchpad.net/bugs/861296
    
    BugLink: http://bugs.launchpad.net/bugs/861296
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Acked-by: Nicolas Pitre <nico@linaro.org>
    
    Backported from 7dbaa466780a754154531b44c2086f6618cee3a8
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>
    Acked-by: Seth Forshee <seth.forshee@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- arch/arm/include/asm/pgtable.h
+++ arch/arm/include/asm/pgtable.h
@@ -446,6 +446,7 @@ extern pgd_t swapper_pg_dir[PTRS_PER_PGD];
  * We provide our own arch_get_unmapped_area to cope with VIPT caches.
  */
 #define HAVE_ARCH_UNMAPPED_AREA
+#define HAVE_ARCH_UNMAPPED_AREA_TOPDOWN
 
 /*
  * remap a physical page `pfn' of size `size' with page protection `prot'
