a004dbe3dbde230a35e46acc3d1ac6e583c8ceba
Colin Cross <ccross@android.com>
2012-02-01 16:02:23 -0800

    printk: update log_buf_copy for 3.3
    logbuf_lock is a raw spinlock in 3.3, replace spin_(un)lock_irq
    with raw_spin_(un)lock_irq.
    
    Change-Id: I4b13c6a34bcbcbe95ef28ac3aec0528deef7fa9d
    Signed-off-by: Colin Cross <ccross@android.com>
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- kernel/printk.c
+++ kernel/printk.c
@@ -788,7 +788,7 @@ int log_buf_copy(char *dest, int idx, int len)
 	bool took_lock = false;
 
 	if (!oops_in_progress) {
-		spin_lock_irq(&logbuf_lock);
+		raw_spin_lock_irq(&logbuf_lock);
 		took_lock = true;
 	}
 
@@ -805,7 +805,7 @@ int log_buf_copy(char *dest, int idx, int len)
 	}
 
 	if (took_lock)
-		spin_unlock_irq(&logbuf_lock);
+		raw_spin_unlock_irq(&logbuf_lock);
 
 	return ret;
 }
