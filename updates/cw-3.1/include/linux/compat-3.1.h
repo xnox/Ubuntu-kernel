c00db4be6c43b0436b648ea5cc5163fb44629986
James M. Leddy <james.leddy@canonical.com>
2012-02-22 14:37:16 -0500

    UBUNTU: Implement dummy security_sk_clone for compat-wireless
    BugLink: https://bugs.launchpad.net/bugs/914689
    
    From the upstream commit:
    
      This has been defined in include/linux/security.h for some time, but
      was only given an EXPORT_SYMBOL for 3.1.  Add a compat_* definition
      to avoid breaking the module load with an undefined symbol.
    
    backport of compat :
      545ece91974440f6447bbd27a44775bdaeee8212
      0c51ea9ee6ad49b34dadf92798f7109f12cd0490
    
    Signed-off-by: James M. Leddy <james.leddy@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>

--- updates/cw-3.1/include/linux/compat-3.1.h
+++ updates/cw-3.1/include/linux/compat-3.1.h
@@ -22,6 +22,8 @@
 #define br_port_exists(dev)	(dev->br_port)
 #endif
 
+#include <linux/security.h>
+
 #define genl_dump_check_consistent(cb, user_hdr, family)
 
 #define IFF_TX_SKB_SHARING	0x10000	/* The interface supports sharing
@@ -35,6 +37,17 @@
 	.card_id = (card), \
 	.prod_id = { NULL, NULL, (v3), NULL }, \
 	.prod_id_hash = { 0, 0, (vh3), 0 }, }
+
+/*
+ * This has been defined in include/linux/security.h for some time, but was
+ * only given an EXPORT_SYMBOL for 3.1.  Add a compat_* definition to avoid
+ * breaking the compile.
+ */
+#define security_sk_clone(a, b) compat_security_sk_clone(a, b)
+
+static inline void security_sk_clone(const struct sock *sk, struct sock *newsk)
+{
+}
 
 #endif /* (LINUX_VERSION_CODE < KERNEL_VERSION(3,1,0)) */
 
