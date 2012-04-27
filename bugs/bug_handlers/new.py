2912c7123eeafe7e3a63c3ebaa1e7ed79f18c920
Brad Figg <brad.figg@canonical.com>
2012-04-27 08:27:03 -0700

    work ProblemType: Package bugs when 'New'
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- bugs/bug_handlers/new.py
+++ bugs/bug_handlers/new.py
@@ -4,5 +4,6 @@
 from new_hands_off                      import NewHandsOff
 from required_logs                      import RequiredLogs
 from add_series_tag                     import AddSeriesTag
+from problem_type_package               import ProblemTypePackage
 
 # vi:set ts=4 sw=4 expandtab:
