cc962c4659ab3769902af9e207b80db3387dc1d2
Subramaniam Chanderashekarapuram <subramaniam.ca@ti.com>
2012-06-07 16:23:56 +0800

    cma: Fix CMA checkpatch warnings
    This patch fixes checkpatch warnings existing in the cma patchset
    as of v3.4_rc5. This is maintained as an incremental patch on top
    of the original cma patchset to facilitate tracking cma patches.
    
    Signed-off-by: Subramaniam Chanderashekarapuram <subramaniam.ca@ti.com>
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- arch/arm/mm/mmu.c
+++ arch/arm/mm/mmu.c
@@ -916,7 +916,7 @@ static int __init early_vmalloc(char *arg)
 }
 early_param("vmalloc", early_vmalloc);
 
-phys_addr_t arm_lowmem_limit __initdata = 0;
+phys_addr_t arm_lowmem_limit __initdata;
 
 void __init sanity_check_meminfo(void)
 {
@@ -999,7 +999,8 @@ void __init sanity_check_meminfo(void)
 			bank->size = newsize;
 		}
 #endif
-		if (!bank->highmem && bank->start + bank->size > arm_lowmem_limit)
+		if (!bank->highmem &&
+				bank->start + bank->size > arm_lowmem_limit)
 			arm_lowmem_limit = bank->start + bank->size;
 
 		j++;
